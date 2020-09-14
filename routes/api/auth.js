a66d31bc93451caf2d4cfa0aca464ea5e887aaef
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-14 14:45:29 +0900

    add user authentication and login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
@@ -26,42 +27,28 @@ router.get('/', auth, async (req, res) => {
 // @desc Authenticate user & get token
 // @access Public
 router.post('/', [
-    check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    check('password', 'Password is required').exists()
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
     
     try {
         let user = await User.findOne({ email });
 
-        if (user) {
-            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
         }
 
-        const avatar = gravater.url(email, {
-            s: '200',
-            r: 'pg',
-            d: 'mm'
-        });
+        const isMatch = await bcrypt.compare(password, user.password);
 
-        user = new User({
-            name,
-            email,
-            avatar,
-            password
-        });
-
-        const salt = await bcrypt.genSalt(10);
-
-        user.password = await bcrypt.hash(password, salt);
-        
-        await user.save();
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
 
         const payload = {
             user: {
