725a7b5bae193665838db7e27a246764c46ca9fc
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-14 21:17:53 +0900

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,10 +28,10 @@ router.get('/me', auth, async (req, res) => {
 // @route POST  api/profile
 // @desc Create or update user profile
 // @access Private
-router.post('/', auth, [
+router.post('/', [ auth, [
     check('status', 'Status is required').not().isEmpty(),
     check('skills', 'Skills is required').not().isEmpty()
-], async (req, res) => {
+] ], async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -147,6 +147,57 @@ router.delete('/', auth, async (req, res) => {
         await User.findOneAndRemove({ _id: req.user.id });
 
         res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route PUT  api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/experience', [
+    auth, [
+        check('title', 'Title is required').not().isEmpty(),
+        check('company', 'Company is required').not().isEmpty(),
+        check('from', 'From date is required').not().isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
     } catch (err) {
         console.error(err.message);
 
