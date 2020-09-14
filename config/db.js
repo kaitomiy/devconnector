fb88839b957a3b5fa32e1cef5b9ae978dd3153d5
Kaito Mitomi <kaitomiy@gmail.com>
2020-09-14 11:36:31 +0900

    add routes and connect to database

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongooose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongooose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
